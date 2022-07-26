338b11fe84ebadb1e312c9b631bbbd31111ab065
Malaika Afridi <malaikaafridi22@gmail.com>
2022-07-26 10:36:59 -0700

    style: styled wizard header

--- src/components/views/SellCar/WizardPostAd/HeaderWizard.tsx
+++ src/components/views/SellCar/WizardPostAd/HeaderWizard.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const HeaderWizard = ({ activeIndex }: { activeIndex: number }) => {
   return (
     <div>
-      <h2>Sell Your Card</h2>
-      <div className='h-10 w-full relative flex flex-col justify-center'>
+      <h2 className='text-2xl font-bold'>Sell Your Car</h2>
+      <div className='h-10 max-w-5xl mx-auto relative flex flex-col justify-center mt-5'>
         <div className='h-[6px] bg-[#C4C4C4] rounded-md'></div>
         <div
           className={`h-[6px] rounded-md absolute  transition-all duration-300 ease-in-out bg-[#54D406] w-0  ${
@@ -15,7 +15,7 @@ const HeaderWizard = ({ activeIndex }: { activeIndex: number }) => {
               : activeIndex === 3 && "w-[100%]"
           }`}
         >
-          <div className='h-5 w-5 absolute top-0 right-0'>
+          <div className='h-6 w-10 absolute top-[-6px] right-[-30px]'>
             <Image src={"/images/wizardCar.png"} layout='fill' />
           </div>
         </div>
