70e3aa67225b3a63f97f3790d361ef70cb55cadf
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-02 12:35:54 -0700

    feat: header profile dropdown

--- src/components/modules/Layout/Header/Header.tsx
+++ src/components/modules/Layout/Header/Header.tsx
@@ -1,5 +1,8 @@
+import { Menu, Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import { AiFillCaretDown } from 'react-icons/ai';
 
 import { Anchor, Logo } from '@/modules';
 
@@ -31,7 +34,9 @@ const Header = () => {
         <Logo />
       </div>
       <div className='text-white grow text-end flex-1'>
-        <span className='text-[14px]'>Welcome Yusra</span>
+        <span className='text-[14px]'>
+          <ProfileDropdown />
+        </span>
         <Anchor
           href='post-ad'
           className='bg-gradient-to-r from-[#EF6212] to-[#D14B00] ml-6 text-[14px] rounded-md px-5 py-2'
@@ -54,3 +59,49 @@ const NavItems = ({ href, text }: { href: string; text: string }) => {
   );
 };
 export default Header;
+
+const ProfileDropdown = () => {
+  const router = useRouter();
+  const handleRoute = () => {
+    router.push('/profile');
+  };
+  return (
+    <Menu as='div' className='relative inline-block text-left'>
+      <div>
+        <Menu.Button className='inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
+          Welcome Yusra
+          <AiFillCaretDown
+            className='ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100'
+            aria-hidden='true'
+          />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter='transition ease-out duration-100'
+          enterFrom='transform opacity-0 scale-95'
+          enterTo='transform opacity-100 scale-100'
+          leave='transition ease-in duration-75'
+          leaveFrom='transform opacity-100 scale-100'
+          leaveTo='transform opacity-0 scale-95'
+        >
+          <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+            <div className='px-1 py-1 '>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-[#EF6212] text-white' : 'text-gray-900'
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    onClick={handleRoute}
+                  >
+                    Profile
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </div>
+    </Menu>
+  );
+};
