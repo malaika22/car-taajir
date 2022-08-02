8ea39412c93354757131e7d8d5884943f022fe22
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-02 12:32:02 -0700

    feat: added profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import { ProfileInfo } from '@views';
+
+const ProfilePage = () => {
+  return (
+    <div className='bg-[#E5E5E5] py-7'>
+      <div className='max-w-7xl mx-auto'>
+        <ProfileInfo />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
