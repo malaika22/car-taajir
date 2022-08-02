30eaa7d1e2c464c489bd5b70baadefadb09e584c
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-02 09:08:43 -0700

    feat: added specifications component in ad preview

---
+++ src/components/views/SellCar/WizardPostAd/PreviewAd/Specifications.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+
+const Specifications = () => {
+  const specifications = [
+    {
+      value: '92,000km',
+      icon: '/images/previewMock/icons/specifications/speedometer.png',
+    },
+    {
+      value: 'Petrol',
+      icon: '/images/previewMock/icons/specifications/gas-station.png',
+    },
+    {
+      value: 'Automatic',
+      icon: '/images/previewMock/icons/specifications/gear-box.png',
+    },
+    {
+      value: '2015',
+      icon: '/images/previewMock/icons/specifications/sedan.png',
+    },
+    {
+      value: 'Air bag',
+      icon: '/images/previewMock/icons/specifications/air-bag.png',
+    },
+    {
+      value: 'Power Steering',
+      icon: '/images/previewMock/icons/specifications/steering-wheel.png',
+    },
+    {
+      value: '2017',
+      icon: '/images/previewMock/icons/specifications/calender.png',
+    },
+  ];
+  return (
+    <div className='border-[#D8D5D5] border-[2px] rounded-[25px] p-10 gap-x-3 my-10'>
+      <div className='grid grid-cols-7'>
+        {specifications.map((specification) => (
+          <div key={specification.value} className='text-center'>
+            <div className='relative w-14 h-14 mx-auto'>
+              <Image
+                src={specification.icon}
+                layout='fill'
+                alt={specification.value}
+              />
+            </div>
+            <span className='mt-2 inline-block font-bold'>
+              {specification.value}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Specifications;
