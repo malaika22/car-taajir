7c626ed2dc50d9ef901b35104a8e37cc21c1c2ca
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-02 12:35:26 -0700

    added absolute paths in index.tsx

--- src/components/views/index.tsx
+++ src/components/views/index.tsx
@@ -1,6 +1,7 @@
 export { default as HomeCarousel } from './Home/HomeCarousel';
-export { default as ShopByStyle } from './NewCars/index';
-export { default as FeaturedCars } from './UsedCars/index';
-export { default as SignUp } from './SignUp/index';
 export { default as Login } from './Login/index';
+export { default as ShopByStyle } from './NewCars/index';
+export { ProfileAds, ProfileInfo } from './Profile/index';
 export { default as WizardPostAd } from './SellCar/index';
+export { default as SignUp } from './SignUp/index';
+export { default as FeaturedCars } from './UsedCars/index';
