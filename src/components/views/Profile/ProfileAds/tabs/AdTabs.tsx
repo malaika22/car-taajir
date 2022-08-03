a0525a187fbd53dcd67fee16edcfe0bd4fc42afe
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-03 15:12:34 -0700

    feat: added tabs preview in Ads component

---
+++ src/components/views/Profile/ProfileAds/tabs/AdTabs.tsx
@@ -0,0 +1,62 @@
+import { Tab } from '@headlessui/react';
+import { AiFillHeart } from 'react-icons/ai';
+import { ImBubble } from 'react-icons/im';
+
+import Messages from './Messages';
+import MyAds from './MyAds';
+import SavedAds from './SavedAds';
+
+const AdTabs = () => {
+  const tabs = [
+    { Icon: '', title: 'My Ads' },
+    { Icon: AiFillHeart, title: 'Saved Ads' },
+    { Icon: ImBubble, title: 'Messages' },
+  ];
+  const components = [MyAds, SavedAds, Messages];
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+  }
+
+  return (
+    <div className='py-10'>
+      <Tab.Group>
+        <Tab.List className='flex space-x-3 w-[500px] mx-auto'>
+          {tabs.map(({ Icon, title }, index) => (
+            <Tab
+              key={title}
+              className={({ selected }) =>
+                classNames(
+                  'w-full rounded-lg py-2.5  leading-5 text-black-700 hover:bg-gradient-to-r from-[#EF8A12] to-[#F66800] font-bold text-md',
+                  'ring-white ring-opacity-60 ring-offset-2  border-[#EF8A12] border-2 focus:outline-none focus:ring-2',
+                  selected
+                    ? 'bg-gradient-to-r from-[#EF8A12] to-[#F66800]  text-white'
+                    : ' text-black hover:bg-white/[0.12] hover:text-white'
+                )
+              }
+            >
+              <div className='flex items-center justify-center py-2'>
+                {Icon && <Icon className={`mr-2 text-[#F37708] `} />} {title}
+              </div>
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className='mt-2'>
+          {components.map((Component, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames('rounded-xl bg-white p-3')}
+            >
+              <div className='bg-[#F3F3F3] p-8 rounded-xl'>
+                {' '}
+                <Component />
+              </div>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  );
+};
+
+export default AdTabs;
