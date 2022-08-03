87dcf790cc96338378375f786d9bdfb7fb75355e
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-03 15:15:58 -0700

    feat: added saved ads feature

---
+++ src/components/views/Profile/ProfileAds/tabs/SavedAds.tsx
@@ -0,0 +1,21 @@
+import { myAds } from 'utils/constant';
+
+import AdCard from '../AdCard';
+
+const SavedAds = () => {
+  return (
+    <>
+      <div className='text-[#8B8B8B] text-end mb-3 text-md font-bold'>
+        Showing 01 Ads
+      </div>
+
+      <div className='space-y-7'>
+        {myAds.map((ad, index) => (
+          <AdCard status='Make it Active Again' ad={ad} key={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SavedAds;
