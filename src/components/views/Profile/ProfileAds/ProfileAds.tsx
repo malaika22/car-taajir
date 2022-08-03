4d7d8d576b27a5de2d05e8efd4911029a0726798
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-03 15:12:17 -0700

    feat: implemented profile ads component

---
+++ src/components/views/Profile/ProfileAds/ProfileAds.tsx
@@ -0,0 +1,16 @@
+import AdTabs from './tabs/AdTabs';
+
+const ProfileAds = () => {
+  return (
+    <div className='bg-white py-12 px-8 rounded-[18px] shadow-[0px_4px_4px_0rem_rgb(0,0,0,0.25)]'>
+      <h2 className='text-center grow text-[#444343] font-bold text-2xl'>
+        My Ads
+      </h2>
+      <div className=' px-16'>
+        <AdTabs />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileAds;
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,12 @@
-import { ProfileInfo } from '@views';
+import { ProfileAds, ProfileInfo } from '@views';
+
 
 const ProfilePage = () => {
   return (
     <div className='bg-[#E5E5E5] py-7'>
-      <div className='max-w-7xl mx-auto'>
+      <div className='max-w-7xl mx-auto space-y-12'>
         <ProfileInfo />
+        <ProfileAds />
       </div>
     </div>
   );
