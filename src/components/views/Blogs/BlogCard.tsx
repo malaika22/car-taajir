af3b9b1eb86e5554890fb757698f4a19d019eb31
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-07 04:14:29 -0700

    fix: updated blog card

--- src/components/views/Blogs/BlogCard.tsx
+++ src/components/views/Blogs/BlogCard.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineRight } from 'react-icons/ai';
 
 interface IBlogCardProps {
   blog: {
@@ -13,7 +15,7 @@ interface IBlogCardProps {
 
 const BlogCard = ({ blog }: IBlogCardProps) => {
   return (
-    <div className='bg-white p-4 rounded-lg ring-1 ring-offset-2 ring-slate-50'>
+    <div className='bg-white p-4 rounded-lg ring-1 ring-offset-2 ring-slate-50 hover:ring-[#EF6212] cursor-pointer'>
       <div className='w-full h-72 relative'>
         <Image
           src={blog.mainImage}
@@ -28,7 +30,15 @@ const BlogCard = ({ blog }: IBlogCardProps) => {
         </div>
 
         <h2 className='text-lg font-bold my-4'>{blog.title}</h2>
-        <div className='h-[240px] textOverflow '>{blog.shortDescription}</div>
+        <div>{blog.shortDescription}</div>
+      </div>
+      <div className='text-[#EF6212] font-bold text-sm group cursor-pointer inline-block'>
+        <Link href={`/blogs/${blog.id}`}>
+          <div className='flex items-center'>
+            Read article{' '}
+            <AiOutlineRight className='mr-2 group-hover:translate-x-[8px] transition-all ease-in-out duration-[.15s]' />
+          </div>
+        </Link>
       </div>
     </div>
   );
