67411fd1351af098cd119bf735ccf65c5c38d79a
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-15 23:30:30 +0500

    stlyle: made status wizard page responsive

--- src/components/views/SellCar/WizardPostAd/StatusWizard.tsx
+++ src/components/views/SellCar/WizardPostAd/StatusWizard.tsx
@@ -1,4 +1,3 @@
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { FiCheck, FiX } from 'react-icons/fi';
 import { STATUS } from 'types/general.types';
 
@@ -16,33 +15,42 @@ interface IStatusWizard {
 const StatusWizard = ({ formState: { status, data } }: IStatusWizard) => {
   const headings = {
     [STATUS.loading]: {
-      title: data ? data.title : 'Loading..',
-      subtitle: data ? data.subtitle : 'Loading..',
+      title: data?.title ? data.title : 'Loading..',
+      subtitle: data?.title ? data.subtitle : 'Loading..',
       Icon: () => (
-        <div className='p-4 rounded-full bg-slate-100 '>
-          {data?.Icon}
-          {/* <Icon className=' text-5xl sm:text-6xl text-slate-400  animate-bounce   ' /> */}
+        <div className='p-9 rounded-full bg-slate-100 '>
+          {data?.Icon ? (
+            data?.Icon
+          ) : (
+            <Icon className=' text-5xl sm:text-7xl text-slate-400  animate-bounce   ' />
+          )}
         </div>
       ),
     },
     [STATUS.success]: {
-      title: 'Your ad was submitted successfully',
-      subtitle: 'Please while we prepare your ad preview',
+      title: data?.title ? data.title : 'Your ad was submitted successfully',
+      subtitle: data?.subtitle
+        ? data.subtitle
+        : 'Please while we prepare your ad preview',
 
       Icon: () => (
         <div className='p-4 rounded-full bg-green-400'>
-          <AiOutlineLoading3Quarters className='animate-spin' />
           <FiCheck className=' text-5xl sm:text-7xl text-green-900   animate-pulse ' />
         </div>
       ),
     },
     [STATUS.error]: {
-      title: 'Something went wrong',
-      subtitle:
-        'Please try again and if the issues persists, Try contacting us.',
+      title: data?.title ? data.title : 'Something went wrong',
+      subtitle: data?.subtitle
+        ? data.subtitle
+        : 'Please try again and if the issues persists, Try contacting us.',
       Icon: () => (
         <div className='p-4 rounded-full bg-amber-200'>
-          <FiX className=' text-5xl sm:text-7xl text-amber-900   animate-pulse ' />
+          {data?.Icon ? (
+            data.Icon
+          ) : (
+            <FiX className=' text-5xl sm:text-7xl text-amber-900   animate-pulse ' />
+          )}
         </div>
       ),
     },
@@ -60,9 +68,14 @@ const StatusWizard = ({ formState: { status, data } }: IStatusWizard) => {
 
   const Icon = headings[status].Icon;
   return (
-    <div>
-      <div>{headings[status].title}</div>
-      <div>{headings[status].subtitle}</div>
+    <div className='min-h-[400px] flex flex-col items-center justify-center px-10 text-center'>
+      <div className='text-[#939393] text-2xl mb-10 font-bold'>
+        {headings[status].title}
+      </div>
+      <div className='text-lg font-semibold mb-10'>
+        {' '}
+        {headings[status].subtitle}
+      </div>
       <Icon />
     </div>
   );
