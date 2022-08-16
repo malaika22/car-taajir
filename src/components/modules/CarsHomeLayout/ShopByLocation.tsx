792c5c175c7b991b01d190e084f0bae6d96dbdfe
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-16 23:49:12 +0500

    style: Shop by location component responsive

--- src/components/modules/CarsHomeLayout/ShopByLocation.tsx
+++ src/components/modules/CarsHomeLayout/ShopByLocation.tsx
@@ -2,12 +2,12 @@ import { shopByLocation } from 'utils/constant';
 
 const ShopByLocation = () => {
   return (
-    <div className='bg-[#E5E5E5]'>
+    <div className='bg-[#E5E5E5]  px-5'>
       <div className='max-w-5xl mx-auto py-20 '>
-        <h2 className='text-[#444343] text-lg font-bold mb-5'>
+        <h2 className='text-[#444343] text-lg font-bold mb-5 text-center sm:text-start'>
           Shop by Location
         </h2>
-        <div className='grid grid-cols-5 gap-x-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 gap-x-4 gap-y-5 '>
           {shopByLocation.map((location) => (
             <div
               key={location.city}
