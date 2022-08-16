a014e579e7c03a343af0e16690cf4153b1af02a8
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-16 23:48:37 +0500

    style: Car Categories component responsive

--- src/components/modules/CarsHomeLayout/CarCategories/CarCategories.tsx
+++ src/components/modules/CarsHomeLayout/CarCategories/CarCategories.tsx
@@ -3,7 +3,7 @@ import TypeCategory from './TypeCategory';
 
 const CarCategories = () => {
   return (
-    <div className='bg-[#E5E5E5] pt-20 pb-20'>
+    <div className='bg-[#E5E5E5] pt-28 pb-20'>
       <div className='max-w-5xl mx-auto'>
         <TypeCategory />
         <BrandCategory />
