31048d2ed63bb6a09c0c83ee1e831b8a34cae020
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-19 02:10:11 +0500

    style: find-new-car page responsive

--- src/pages/find-new-cars/car/[id].tsx
+++ src/pages/find-new-cars/car/[id].tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
-import { AiOutlineDislike, AiOutlineLike } from 'react-icons/ai';
+import { useState } from 'react';
+import {
+  AiFillCaretRight,
+  AiOutlineDislike,
+  AiOutlineLike,
+} from 'react-icons/ai';
+import { FiX } from 'react-icons/fi';
 import { IoLocationSharp } from 'react-icons/io5';
 import ReactStars from 'react-rating-stars-component';
 
@@ -50,7 +56,7 @@ const info = {
 const NewCar = () => {
   return (
     <div className='bg-[#E5E5E5] flex relative'>
-      <div className='max-w-6xl mx-auto py-10 pb-32 '>
+      <div className='max-w-6xl mx-auto py-10 pb-32 px-5'>
         <div className='pl-[60px]'>
           <h2 className='text-2xl font-bold'>2015 Toyota Corolla LE</h2>
           <div className='flex items-center text-sm mt-1 mb-6'>
@@ -58,11 +64,11 @@ const NewCar = () => {
             <span className='text-[#B5B5B5]'>Karachi, Pakistan</span>
           </div>
         </div>
-        <div className='w-full min-h-[500px] h-[500px] pl-[60px]'>
+        <div className='w-full min-h-[500px] h-[500px] mdLg:pt-0 pt-10 mdLg:pl-[60px]'>
           <ImagesCarousel />
         </div>
         {/* Specification */}
-        <div className='grid grid-cols-6 px-7 py-10 my-10 max-w-3xl mx-auto bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+        <div className='grid grid-cols-3 gap-y-8 sm:grid-cols-6 px-7 py-10 my-10 max-w-3xl mx-auto bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
           {specifications.map((specification, index) => (
             <div key={index} className='text-center'>
               <div className='h-10 w-10 relative mx-auto'>
@@ -72,7 +78,7 @@ const NewCar = () => {
                   layout='fill'
                 />
               </div>
-              <div className='font-sm font-bold mt-2'>
+              <div className='font-sm font-semibold mt-2'>
                 {specification.value}
               </div>
             </div>
@@ -81,11 +87,11 @@ const NewCar = () => {
         {/* Specification */}
 
         {/* Car Information */}
-        <div className='my-10 py-12 px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
           <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
             Honda Civic 2022 price in Pakistan
           </h2>
-          <div className='grid grid-cols-3 gap-x-6'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-y-8 md:grid-cols-3 gap-x-6'>
             {Array(3)
               .fill({})
               .map((_, i) => (
@@ -96,28 +102,28 @@ const NewCar = () => {
         {/* Car Information */}
 
         {/* Pros Cons */}
-        <div className='my-10 py-12 px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
           <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
             Pros & Cons
           </h2>
-          <div className='grid grid-cols-2'>
-            <div className='w-[300px] mx-auto'>
+          <div className='grid gap-x-4 gap-y-8 sm:grid-cols-2'>
+            <div className='w-full sm:max-w-xs mx-auto'>
               <AiOutlineLike className='mx-auto text-5xl text-[#3C9E00] mb-7' />
 
-              <ul className='list-disc leading-7 text-sm'>
+              <ul className='list-disc leading-7 text-sm break-words'>
                 {info['pros'].map((info, index) => (
-                  <li key={index} className='text-[#444343] font-medium'>
+                  <li key={index} className='text-[#444343] font-medium '>
                     {info}
                   </li>
                 ))}
               </ul>
             </div>
-            <div className='w-[300px] mx-auto '>
+            <div className='w-full sm:max-w-xs  mx-auto '>
               <AiOutlineDislike className='mx-auto text-5xl text-[#F6000F] mb-7' />
 
-              <ul className='list-disc  leading-7 text-sm'>
+              <ul className='list-disc  leading-7 text-sm break-words'>
                 {info['cons'].map((info, index) => (
-                  <li key={index} className='text-[#444343] font-medium'>
+                  <li key={index} className='text-[#444343] font-medium '>
                     {info}
                   </li>
                 ))}
@@ -128,7 +134,7 @@ const NewCar = () => {
         {/* Pros Cons */}
 
         {/* Overview */}
-        <div className='my-10 py-12 px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
           <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
             Overview
           </h2>
@@ -159,13 +165,13 @@ const NewCar = () => {
         {/* Overview */}
 
         {/* Review */}
-        <div className='my-10 py-12 px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
           <Reviews />
         </div>
         {/* Review */}
 
         {/* Colors */}
-        <div className='mt-10 py-12 px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+        <div className='mt-10 py-12  px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
           <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
             Colors
           </h2>
@@ -196,7 +202,7 @@ const CarVarient = () => {
     },
   ];
   return (
-    <div className='bg-[#F4F4F4] ring-1 ring-offset-2 ring-slate-50 rounded-lg text-center py-10'>
+    <div className='bg-[#F4F4F4] ring-1 ring-offset-2 ring-slate-50 rounded-lg text-center py-10 xs:px-0 px-2'>
       <div className='text-[#0C45B2] font-semibold mb-4'>
         Honda Civic Standard
       </div>
@@ -209,12 +215,10 @@ const CarVarient = () => {
       <div className='grid grid-cols-3 mt-10'>
         {variants.map((variant, index) => (
           <div key={index}>
-            <Image
-              width={40}
-              height={40}
-              src={variant.icon}
-              alt={variant.value}
-            />
+            <div className='w-8 h-8 mdLg:w-10 mdLg:h-10 relative mx-auto'>
+              <Image layout='fill' src={variant.icon} alt={variant.value} />
+            </div>
+
             <div className='text-sm font-bold text-[#0C45B2]'>
               {variant.value}
             </div>
@@ -227,7 +231,7 @@ const CarVarient = () => {
 
 const Reviews = () => {
   return (
-    <div className='grid grid-cols-2'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 text-center sm:text-start'>
       <div className='w-full mx-auto min-h-[300px] relative'>
         <Image
           src='/images/new-cars/carMock.png'
@@ -239,23 +243,30 @@ const Reviews = () => {
       <div>
         <h2 className='text-[#444343] font-bold text-2xl  mb-5'>Reviews</h2>
         <div className='text-[#0C45B2] font-bold '>Honda Civic Oriel</div>
-        <ReactStars
-          count={5}
-          size={24}
-          activeColor='#ffd700'
-          edit={false}
-          value={3}
-        />
-        <div className='flex space-x-10'>
-          <p className='font-bold pt-5'>
+        <div className='flex sm:justify-start justify-center'>
+          <ReactStars
+            count={5}
+            size={24}
+            activeColor='#ffd700'
+            edit={false}
+            value={3}
+          />
+        </div>
+
+        <div className='flex md:space-x-10'>
+          <p className='font-semibold pt-5 px-3 sm:px-0  sm:text-base text-sm'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pretium
             viverra nunc, gravida neque turpis diam aliquet. Netus aliquam magna
             arcu, suspendisse vitae eget. Risus vitae nunc, pharetra quis
             phasellus ut in nunc. Nunc, ornare augue amet vitae sem vel nunc
             diam nibh. - Anonymous
           </p>
-          <div className='w-[600px] h-[150px] relative'>
-            <Image src='/images/new-cars/quotes.png' layout='fill' />
+          <div className='w-[200px] h-[40px] md:w-[600px] md:h-[150px] relative'>
+            <Image
+              src='/images/new-cars/quotes.png'
+              layout='fill'
+              alt='quote'
+            />
           </div>
         </div>
       </div>
@@ -295,14 +306,14 @@ const Colors = () => {
     },
   ];
   return (
-    <div className='grid grid-cols-7 gap-x-6'>
+    <div className='grid gap-y-8 grid-cols-3 sm:grid-cols-4 md:grid-cols-7 gap-x-6'>
       {colors.map((color, index) => (
         <div key={index}>
           <div
-            className='h-[134px] w-full rounded-[50%] shadow-[0px_4px_4px_0rem_rgb(0,0,0,0.25)]'
+            className='pb-[100%]   rounded-[50%] shadow-[0px_4px_4px_0rem_rgb(0,0,0,0.25)]'
             style={{ backgroundColor: color.value }}
           ></div>
-          <div className='text-center text-[#444343] font-bold mt-3'>
+          <div className='text-center text-[#444343] md:text-base text-sm  font-semibold mt-3'>
             {color.name}
           </div>
         </div>
@@ -312,14 +323,40 @@ const Colors = () => {
 };
 
 const SideMenu = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className='w-[200px] h-[350px] bg-black text-center absolute top-12 rounded-r-[20px] py-6'>
-      <div className=' text-white text-lg mb-6'>Honda Civic</div>
-      <div className='font-bold text-white my-4'>Price</div>
-      <div className='font-bold text-white my-4'>Pros & Cons</div>
-      <div className='font-bold text-white my-4'>Overview</div>
-      <div className='font-bold text-white my-4'>Reviews</div>
-      <div className='font-bold text-white my-4'>Colors</div>
-    </div>
+    <>
+      <div className='w-[200px] h-[350px]  hidden mdLg:block bg-black text-center absolute top-12 rounded-r-[20px] py-6'>
+        <div className=' text-white text-lg mb-6'>Honda Civic</div>
+        <div className='font-bold text-white my-4'>Price</div>
+        <div className='font-bold text-white my-4'>Pros & Cons</div>
+        <div className='font-bold text-white my-4'>Overview</div>
+        <div className='font-bold text-white my-4'>Reviews</div>
+        <div className='font-bold text-white my-4'>Colors</div>
+      </div>
+      <div className='block mdLg:hidden text-center absolute top-12 rounded-r-[20px]  bg-white px-[10px] py-[30px]'>
+        <AiFillCaretRight
+          className='text-black text-xl'
+          onClick={() => setShowMenu(!showMenu)}
+        />
+      </div>
+
+      {showMenu && (
+        <div className='w-[200px] h-[280px] block mdLg:hidden   bg-black text-center absolute top-12 rounded-r-[20px] py-3'>
+          <div className='text-end mr-4 cursor-pointer'>
+            <FiX
+              onClick={() => setShowMenu(false)}
+              className='text-white inline-block'
+            />
+          </div>
+          <div className=' text-white mb-6'>Honda Civic</div>
+          <div className='font-bold text-white text-sm my-4'>Price</div>
+          <div className='font-bold text-white text-sm my-4'>Pros & Cons</div>
+          <div className='font-bold text-white text-sm my-4'>Overview</div>
+          <div className='font-bold text-white text-sm my-4'>Reviews</div>
+          <div className='font-bold text-white text-sm my-4'>Colors</div>
+        </div>
+      )}
+    </>
   );
 };
