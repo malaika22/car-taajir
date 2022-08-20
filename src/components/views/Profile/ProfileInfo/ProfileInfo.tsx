128f44c2c051480a2609b1dc83a098a2505a697a
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-20 16:34:47 +0500

    style: profile page responsive

--- src/components/views/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/views/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,13 +4,11 @@ import ProfileAvatar from './ProfileAvatar';
 const ProfileInfo = () => {
   return (
     <div className='bg-white py-12 px-8 rounded-[18px] shadow-[0px_4px_4px_0rem_rgb(0,0,0,0.25)]'>
-      <div className='flex items-center'>
-        <h2 className='text-center grow text-[#444343] font-bold text-2xl'>
-          My Profile
-        </h2>
-        <div className='text-[#0C45B2]'>Resent Verification Email</div>
-      </div>
-      <div className='px-16'>
+      <h2 className='text-center grow text-[#444343] font-bold text-2xl mb-5'>
+        My Profile
+      </h2>
+
+      <div className='md:px-16'>
         <ProfileAvatar />
         <BasicInformation />
       </div>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,9 @@
 import { ProfileAds, ProfileInfo } from '@views';
 
-
 const ProfilePage = () => {
   return (
     <div className='bg-[#E5E5E5] py-7'>
-      <div className='max-w-7xl mx-auto space-y-12'>
+      <div className='max-w-7xl mx-auto space-y-12 px-5'>
         <ProfileInfo />
         <ProfileAds />
       </div>
