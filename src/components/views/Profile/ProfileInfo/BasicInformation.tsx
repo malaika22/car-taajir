28e08dd9417070e0760d759a30b6d2a47d14c3f9
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-20 16:35:19 +0500

    style: profile basic information responsive

--- src/components/views/Profile/ProfileInfo/BasicInformation.tsx
+++ src/components/views/Profile/ProfileInfo/BasicInformation.tsx
@@ -39,7 +39,7 @@ const BasicInformation = () => {
     <div className='pt-10'>
       <div className='text-xl font-bold'>Basic Information</div>
       <div className='mt-9 mb-4'>
-        <div className='grid grid-cols-3 gap-x-5'>
+        <div className='grid sm:grid-cols-3 gap-x-5'>
           <TextField
             id='fullname'
             placeholder='Enter full name'
@@ -68,7 +68,7 @@ const BasicInformation = () => {
             error={errors.email?.message}
           />
         </div>
-        <div className='w-[349px]'>
+        <div className='sm:w-[349px]'>
           <Select
             id='gender'
             options={gender.map((gender, index) => ({
@@ -82,7 +82,7 @@ const BasicInformation = () => {
             register={register}
           />
         </div>
-        <div className='w-[349px]'>
+        <div className='sm:w-[349px]'>
           <DateInput
             id='date'
             placeholder='Date of birth'
@@ -92,7 +92,7 @@ const BasicInformation = () => {
             required
           />
         </div>
-        <div className='w-[349px]'>
+        <div className='sm:w-[349px]'>
           <Select
             options={country.map((country, index) => ({
               title: country,
@@ -106,7 +106,7 @@ const BasicInformation = () => {
             required
           />
         </div>
-        <div className='w-[349px]'>
+        <div className='sm:w-[349px]'>
           <Select
             options={city.map((city, index) => ({
               title: city,
@@ -123,29 +123,68 @@ const BasicInformation = () => {
       </div>
       <div>
         <div className='text-[#8B8B8B] font-bold mb-4'>Mobile Number</div>
-        <div className='bg-[#F3F3F3] flex py-5 px-8 pt-6 rounded-xl'>
-          <div className='grid grid-cols-4 grid-rows-2 grow'>
+        <div className='bg-[#F3F3F3] py-5  px-4 lg:px-8 pt-6 rounded-xl'>
+          <div className='grid gap-y-6 sm:grid-cols-5'>
+            <div className='text-center'>
+              <div className='text-[#8B8B8B] font-bold mb-3 '>Number</div>
+              <div className='text-[#8B8B8B] text-sm  md:text-base'>
+                +92 300 000 0000
+              </div>
+            </div>
+            <div className='text-center'>
+              <div className='text-[#8B8B8B] font-bold mb-3 '>Status</div>
+              <div className='text-[#8B8B8B] flex md:justify-start  justify-center items-start text-sm  md:text-base'>
+                Verified{' '}
+                <BsFillPatchCheckFill className='text-[#159954] mt-[4px] ml-2 text-sm  md:text-base' />
+              </div>
+            </div>
+
+            <div className='text-center'>
+              <div className='text-[#8B8B8B] font-bold mb-3 '>Remove</div>
+              <div className='text-[#F6000F] font-bold cursor-pointer text-sm  md:text-base'>
+                Remove this number
+              </div>
+            </div>
+            <div className='text-center'>
+              <div className='text-[#8B8B8B] font-bold mb-3 opacity-0 hidden sm:block'>
+                ds
+              </div>
+              <div className='text-[#0C45B2] font-bold cursor-pointer text-sm  md:text-base'>
+                Save as Primary
+              </div>
+            </div>
+            <div className='text-center'>
+              <button className='bg-gradient-to-r from-[#3C9E00] to-[#2C7400] font-bold text-white rounded-lg h-12 text-sm  px-8 sm:px-0 sm:w-full  md:text-base'>
+                Add Numbers
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* <div className='bg-[#F3F3F3] flex sm:flex-row flex-col py-5 px-5 md:px-8 pt-6 rounded-xl'>
+          <div className='grid sm:grid-cols-4 sm:grid-rows-2 grid-rows-1 grow text-center md:text-start'>
             <div className='text-[#8B8B8B] font-bold mb-3 '>Number</div>
             <div className='text-[#8B8B8B] font-bold mb-3 '>Status</div>
             <div className='text-[#8B8B8B] font-bold mb-3 '>Remove</div>
             <div></div>
-            <div className='text-[#8B8B8B]  '>+92 300 000 0000</div>
-            <div className='text-[#8B8B8B] flex items-start '>
-              Verified{' '}
-              <BsFillPatchCheckFill className='text-[#159954] mt-[4px] ml-2' />
+            <div className='text-[#8B8B8B] text-sm  md:text-base'>
+              +92 300 000 0000
             </div>
-            <div className='text-[#F6000F] font-bold cursor-pointer'>
+            <div className='text-[#8B8B8B] flex md:justify-start  justify-center items-start text-sm  md:text-base'>
+              Verified{' '}
+              <BsFillPatchCheckFill className='text-[#159954] mt-[4px] ml-2 text-sm  md:text-base' />
+            </div>
+            <div className='text-[#F6000F] font-bold cursor-pointer text-sm  md:text-base'>
               Remove this number
             </div>
-            <div className='text-[#0C45B2] font-bold cursor-pointer'>
+            <div className='text-[#0C45B2] font-bold cursor-pointer text-sm  md:text-base'>
               Save as Primary
             </div>
             <div></div>
           </div>
-          <button className='bg-gradient-to-r from-[#3C9E00] to-[#2C7400] font-bold text-white rounded-lg h-12 w-52 '>
+          <button className='bg-gradient-to-r from-[#3C9E00] to-[#2C7400] font-bold text-white rounded-lg h-12 md:w-52 text-sm w-36  md:text-base'>
             Add Numbers
           </button>
-        </div>
+        </div> */}
       </div>
       <button className='bg-gradient-to-r from-[#2F69DA] to-[#0C45B2] mt-8 font-bold text-white rounded-lg h-12 w-52 '>
         Save Changes
