a88cc44973f7211692d0f23dfacad18124972d68
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-20 16:35:47 +0500

    style: profile messages responsive

--- src/components/views/Profile/ProfileAds/tabs/Messages.tsx
+++ src/components/views/Profile/ProfileAds/tabs/Messages.tsx
@@ -3,8 +3,53 @@ import { AiFillHeart } from 'react-icons/ai';
 
 const Messages = () => {
   return (
-    <div className='flex justify-between space-x-10 items-center'>
-      <div className='flex max-w-xl items-start'>
+    <div className='grid md:grid-cols-3 gap-y-8  gap-x-4'>
+      <div className='md:col-span-2 grid   xs:grid-cols-3 items-center'>
+        <div className='col-span-1 xs:mb-0 mb-6 w-full h-28 relative mr-4'>
+          <Image
+            src='/images/adMock/messageAvatar.png'
+            layout='fill'
+            alt='avatar'
+            objectFit='contain'
+          />
+        </div>
+        <div className='xs:col-span-2 text-center md:text-start'>
+          <h2 className='text-[#444343] font-bold text-lg mb-2'>Anonymous</h2>
+          <p className='text-[#444343] font-bold text-sm'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget
+            facilisis mattis elementum purus ultricies et, tristique. Vel
+            suspendisse integer nulla arcu.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className='bg-white col-span-1 p-6 py-4'>
+          <div className='border-b-[1px] border-[#D1D1D1] flex mdLg:flex-row flex-col items-center mdLg:items-start justify-between pb-4'>
+            <div className='flex relative'>
+              <div className='w-32 h-[112px] relative'>
+                <Image src='/images/adMock/ad.png' layout='fill' alt='Car ad' />
+              </div>
+              <AiFillHeart className='text-red-500 absolute top-3  inline-block text-lg' />
+            </div>
+            <div className='flex flex-col justify-between mdLg:text-end text-center'>
+              <h2 className='text-[#0C45B2] text-lg font-bold'>Honda Civic</h2>
+              <div className=' text-[#0C45B2] text-xs'>
+                Last Updated:11 April 22
+              </div>
+            </div>
+          </div>
+          <div className='pt-5'>
+            <div className='flex justify-between items-center'>
+              <div className='text-sm'>Reviews</div>
+              <div className='text-[#444343] font-bold text-xs '>
+                0 Total Views
+              </div>
+            </div>
+            <div className='text-[#0C45B2] text-xs'>Karachi, Pakistan</div>
+          </div>
+        </div>
+      </div>
+      {/* <div className='flex max-w-xl  mdLg:flex-row flex-col items-center md:col-span-2'>
         <div className='w-80 h-28 relative mr-4'>
           <Image
             src='/images/adMock/messageAvatar.png'
@@ -22,7 +67,7 @@ const Messages = () => {
           </p>
         </div>
       </div>
-      <div className='bg-white w-96 p-6 py-4'>
+      <div className='bg-white col-span-1 p-6 py-4'>
         <div className='border-b-[1px] border-[#D1D1D1] flex justify-between pb-4'>
           <div className='flex relative'>
             <div className='w-32 h-[112px] relative'>
@@ -45,6 +90,8 @@ const Messages = () => {
           <div className='text-[#444343] font-bold'>0 Total Views</div>
         </div>
       </div>
+      
+      */}
     </div>
   );
 };
