ff1097d2d552666144722fea9ecbb99857c3e7b6
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-20 16:35:31 +0500

    style: profile my ads responsive

--- src/components/views/Profile/ProfileAds/tabs/MyAds.tsx
+++ src/components/views/Profile/ProfileAds/tabs/MyAds.tsx
@@ -21,16 +21,16 @@ const MyAds = () => {
   const tabs = ['Active', 'Pending', 'Removed'];
   return (
     <>
-      <div className='text-[#8B8B8B] text-end mb-3 text-md font-bold'>
+      <div className='text-[#8B8B8B] text-end mb-3  text-sm font-bold'>
         Showing 01 Ads
       </div>
-      <div className='flex items-start space-x-12'>
-        <div>
+      <div className='flex md:flex-row flex-col gap-y-6 items-start gap-x-6'>
+        <div className='md:flex-col flex flex-col sm:flex-row justify-between  md:w-auto w-full '>
           {tabs.map((tab) => (
             <button
               key={tab}
               onClick={() => setSelectedOption(tab)}
-              className={`border-[#326bd9] border-[1px]  cursor-pointer w-40 h-12 rounded-lg font-bold block mb-4 ${
+              className={`border-[#326bd9] border-[1px] text-sm  cursor-pointer w-full sm:w-40 h-12 rounded-lg font-bold block mb-4 ${
                 tab === selectedOption &&
                 'bg-gradient-to-r from-[#0C45B2] to-[#3C74E2] text-white'
               }`}
@@ -39,7 +39,7 @@ const MyAds = () => {
             </button>
           ))}
         </div>
-        <div className='grow'>
+        <div className='w-full md:grow'>
           <MyAdsStatus data={myAds} status={selectedOption} />
         </div>
       </div>
