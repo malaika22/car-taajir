49771864980c5cd6b3f733d0cc6c693e123e88be
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-05 10:50:58 +0500

    feat: added scroll to element

--- src/pages/find-new-cars/car/[id].tsx
+++ src/pages/find-new-cars/car/[id].tsx
@@ -7,8 +7,7 @@ import {
 } from 'react-icons/ai';
 import { FiX } from 'react-icons/fi';
 import { IoLocationSharp } from 'react-icons/io5';
-// eslint-disable-next-line no-var, @typescript-eslint/no-var-requires
-var Rating = require('react-rating');
+import { Rating } from 'react-simple-star-rating';
 
 import { ImagesCarousel } from '@/modules';
 
@@ -56,132 +55,152 @@ const info = {
 
 const NewCar = () => {
   return (
-    <div className='bg-[#E5E5E5] flex relative'>
-      <div className='max-w-6xl mx-auto py-10 pb-32 px-5'>
-        <div className='pl-[60px]'>
-          <h2 className='text-2xl font-bold'>2015 Toyota Corolla LE</h2>
-          <div className='flex items-center text-sm mt-1 mb-6'>
-            <IoLocationSharp className='text-[#5E5E5E] mr-1' />{' '}
-            <span className='text-[#B5B5B5]'>Karachi, Pakistan</span>
-          </div>
-        </div>
-        <div className='w-full min-h-[500px] h-[500px] mdLg:pt-0 pt-10 mdLg:pl-[60px]'>
-          <ImagesCarousel />
-        </div>
-        {/* Specification */}
-        <div className='grid grid-cols-3 gap-y-8 sm:grid-cols-6 px-7 py-10 my-10 max-w-3xl mx-auto bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
-          {specifications.map((specification, index) => (
-            <div key={index} className='text-center'>
-              <div className='h-10 w-10 relative mx-auto'>
-                <Image
-                  src={specification.icon}
-                  alt={specification.value}
-                  layout='fill'
-                />
-              </div>
-              <div className='font-sm font-semibold mt-2'>
-                {specification.value}
-              </div>
+    <>
+      <div className='bg-[#E5E5E5] flex relative'>
+        <div className='max-w-6xl mx-auto py-10 pb-32 px-5'>
+          <div className='pl-[60px]'>
+            <h2 className='text-2xl font-bold'>2015 Toyota Corolla LE</h2>
+            <div className='flex items-center text-sm mt-1 mb-6'>
+              <IoLocationSharp className='text-[#5E5E5E] mr-1' />{' '}
+              <span className='text-[#B5B5B5]'>Karachi, Pakistan</span>
             </div>
-          ))}
-        </div>
-        {/* Specification */}
-
-        {/* Car Information */}
-        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
-          <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
-            Honda Civic 2022 price in Pakistan
-          </h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 gap-y-8 md:grid-cols-3 gap-x-6'>
-            {Array(3)
-              .fill({})
-              .map((_, i) => (
-                <CarVarient key={i} />
-              ))}
           </div>
-        </div>
-        {/* Car Information */}
+          <div className='w-full min-h-[500px] h-[500px] mdLg:pt-0 pt-10 mdLg:pl-[60px]'>
+            <ImagesCarousel />
+          </div>
+          {/* Specification */}
+          <div className='grid grid-cols-3 gap-y-8 sm:grid-cols-6 px-7 py-10 my-10 max-w-3xl mx-auto bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+            {specifications.map((specification, index) => (
+              <div key={index} className='text-center'>
+                <div className='h-10 w-10 relative mx-auto'>
+                  <Image
+                    src={specification.icon}
+                    alt={specification.value}
+                    layout='fill'
+                  />
+                </div>
+                <div className='font-sm font-semibold mt-2'>
+                  {specification.value}
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Specification */}
 
-        {/* Pros Cons */}
-        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
-          <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
-            Pros & Cons
-          </h2>
-          <div className='grid gap-x-4 gap-y-8 sm:grid-cols-2'>
-            <div className='w-full sm:max-w-xs mx-auto'>
-              <AiOutlineLike className='mx-auto text-5xl text-[#3C9E00] mb-7' />
-
-              <ul className='list-disc leading-7 text-sm break-words'>
-                {info['pros'].map((info, index) => (
-                  <li key={index} className='text-[#444343] font-medium '>
-                    {info}
-                  </li>
+          {/* Car Information */}
+          <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
+            <h2
+              className='text-[#444343] font-bold text-2xl text-center mb-11'
+              id='price'
+            >
+              Honda Civic 2022 price in Pakistan
+            </h2>
+            <div className='grid grid-cols-1 sm:grid-cols-2 gap-y-8 md:grid-cols-3 gap-x-6'>
+              {Array(3)
+                .fill({})
+                .map((_, i) => (
+                  <CarVarient key={i} />
                 ))}
-              </ul>
-            </div>
-            <div className='w-full sm:max-w-xs  mx-auto '>
-              <AiOutlineDislike className='mx-auto text-5xl text-[#F6000F] mb-7' />
-
-              <ul className='list-disc  leading-7 text-sm break-words'>
-                {info['cons'].map((info, index) => (
-                  <li key={index} className='text-[#444343] font-medium '>
-                    {info}
-                  </li>
-                ))}
-              </ul>
             </div>
           </div>
-        </div>
-        {/* Pros Cons */}
+          {/* Car Information */}
 
-        {/* Overview */}
-        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
-          <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
-            Overview
-          </h2>
-          <div className='font-bold'>
-            <div className='max-w-3xl mx-auto'>
-              <div>
-                Honda Civic is manufactured by the Pakistani automobile
-                manufacturer and Honda subsidiary Atlas Honda in Pakistan.
-                <br /> The first Honda Civic was launched globally in 1972 and
-                had a 1,169 cc four-cylinder water-cooled engine along with
-                front power disc brakes. It was available in different options
-                such as a two- or four-door fastback sedan, three- and a
-                five-door hatchback, as well as a five-door station wagon. The
-                second-generation Honda Civic was launched in 1979 and was
-                branded as a 1980 model. It had an angular shape, had more
-                interior space and its engine power was better. This generation
-                of Honda Civic was available in three transmissions: a 4-speed
-                and 5-speed manual, and a 2-speed semi-automatic which Honda
-                used to call “Hondamatic”. Previously a subcompact, since 2000
-                the Civic has been categorized as a compact car.
+          {/* Pros Cons */}
+          <div
+            className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'
+            id='pros-cons'
+          >
+            <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
+              Pros & Cons
+            </h2>
+            <div className='grid gap-x-4 gap-y-8 sm:grid-cols-2'>
+              <div className='w-full sm:max-w-xs mx-auto'>
+                <AiOutlineLike className='mx-auto text-5xl text-[#3C9E00] mb-7' />
+
+                <ul className='list-disc leading-7 text-sm break-words'>
+                  {info['pros'].map((info, index) => (
+                    <li key={index} className='text-[#444343] font-medium '>
+                      {info}
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <div className='text-end mt-6 '>
-                <span className='cursor-pointer text-blue-500'>Show more</span>
+              <div className='w-full sm:max-w-xs  mx-auto '>
+                <AiOutlineDislike className='mx-auto text-5xl text-[#F6000F] mb-7' />
+
+                <ul className='list-disc  leading-7 text-sm break-words'>
+                  {info['cons'].map((info, index) => (
+                    <li key={index} className='text-[#444343] font-medium '>
+                      {info}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
-        </div>
-        {/* Overview */}
+          {/* Pros Cons */}
 
-        {/* Review */}
-        <div className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
-          <Reviews />
-        </div>
-        {/* Review */}
+          {/* Overview */}
+          <div
+            className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'
+            id='overview'
+          >
+            <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
+              Overview
+            </h2>
+            <div className='font-bold'>
+              <div className='max-w-3xl mx-auto'>
+                <div>
+                  Honda Civic is manufactured by the Pakistani automobile
+                  manufacturer and Honda subsidiary Atlas Honda in Pakistan.
+                  <br /> The first Honda Civic was launched globally in 1972 and
+                  had a 1,169 cc four-cylinder water-cooled engine along with
+                  front power disc brakes. It was available in different options
+                  such as a two- or four-door fastback sedan, three- and a
+                  five-door hatchback, as well as a five-door station wagon. The
+                  second-generation Honda Civic was launched in 1979 and was
+                  branded as a 1980 model. It had an angular shape, had more
+                  interior space and its engine power was better. This
+                  generation of Honda Civic was available in three
+                  transmissions: a 4-speed and 5-speed manual, and a 2-speed
+                  semi-automatic which Honda used to call “Hondamatic”.
+                  Previously a subcompact, since 2000 the Civic has been
+                  categorized as a compact car.
+                </div>
+                <div className='text-end mt-6 '>
+                  <span className='cursor-pointer text-blue-500'>
+                    Show more
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Overview */}
 
-        {/* Colors */}
-        <div className='mt-10 py-12  px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'>
-          <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
-            Colors
-          </h2>
-          <Colors />
+          {/* Review */}
+          <div
+            className='my-10 py-12 px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'
+            id='reviews'
+          >
+            <Reviews />
+          </div>
+          {/* Review */}
+
+          {/* Colors */}
+          <div
+            className='mt-10 py-12  px-5 sm:px-8 bg-white ring-1 ring-offset-2 ring-slate-50 rounded-lg'
+            id='colors'
+          >
+            <h2 className='text-[#444343] font-bold text-2xl text-center mb-11'>
+              Colors
+            </h2>
+            <Colors />
+          </div>
+          {/* Colors */}
         </div>
-        {/* Colors */}
+        <SideMenu />
       </div>
-      <SideMenu />
-    </div>
+    </>
   );
 };
 
@@ -245,7 +264,7 @@ const Reviews = () => {
         <h2 className='text-[#444343] font-bold text-2xl  mb-5'>Reviews</h2>
         <div className='text-[#0C45B2] font-bold '>Honda Civic Oriel</div>
         <div className='flex sm:justify-start justify-center'>
-          <Rating readonly initialRating={4} stop={5} />
+          <Rating ratingValue={4} transition initialValue={4} />
         </div>
 
         <div className='flex md:space-x-10'>
@@ -319,15 +338,42 @@ const Colors = () => {
 
 const SideMenu = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const handleScroll = (id: string) => {
+    window.scrollTo({
+      top: document.getElementById(id)!.offsetTop - 60,
+      behavior: 'smooth',
+    });
+  };
   return (
     <>
-      <div className='w-[200px] h-[350px]  hidden mdLg:block bg-black text-center absolute top-12 rounded-r-[20px] py-6'>
+      <div className='w-[200px] min-h-[350px]  hidden mdLg:block bg-black text-center absolute top-12 rounded-r-[20px] py-6'>
         <div className=' text-white text-lg mb-6'>Honda Civic</div>
-        <div className='font-bold text-white my-4'>Price</div>
-        <div className='font-bold text-white my-4'>Pros & Cons</div>
-        <div className='font-bold text-white my-4'>Overview</div>
-        <div className='font-bold text-white my-4'>Reviews</div>
-        <div className='font-bold text-white my-4'>Colors</div>
+        <div onClick={() => handleScroll('price')} className='cursor-pointer'>
+          <div className='font-bold text-white my-4 navItems'>Price</div>
+          <div className='borderAnimation h-[1px] w-0 bg-white transition-all duration-300'></div>
+        </div>
+        <div
+          onClick={() => handleScroll('pros-cons')}
+          className='cursor-pointer'
+        >
+          <div className='font-bold text-white my-4 navItems'>Pros & Cons</div>
+          <div className='borderAnimation h-[1px] w-0 bg-white transition-all duration-300'></div>
+        </div>
+        <div
+          onClick={() => handleScroll('overview')}
+          className='cursor-pointer'
+        >
+          <div className='font-bold text-white my-4 navItems'>Overview</div>
+          <div className='borderAnimation h-[1px] w-0 bg-white transition-all duration-300'></div>
+        </div>
+        <div onClick={() => handleScroll('reviews')} className='cursor-pointer'>
+          <div className='font-bold text-white my-4 navItems'>Reviews</div>
+          <div className='borderAnimation h-[1px] w-0 bg-white transition-all duration-300'></div>
+        </div>
+        <div onClick={() => handleScroll('colors')} className='cursor-pointer'>
+          <div className='font-bold text-white my-4 navItems'>Colors</div>
+          <div className='borderAnimation h-[1px] w-0 bg-white transition-all duration-300'></div>
+        </div>
       </div>
       <div className='block mdLg:hidden text-center absolute top-12 rounded-r-[20px]  bg-white px-[10px] py-[30px]'>
         <AiFillCaretRight
