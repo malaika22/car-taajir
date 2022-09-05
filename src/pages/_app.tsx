95a6c3366eabf8e2455524e8ad670410f4da3e16
Malaika Afridi <malaikaafridi22@gmail.com>
2022-09-05 10:51:22 +0500

    feat: get user from localStorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
+import UserWrapper from 'context/UserWrapper';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import { User } from 'types/credentials.types';
 
 import '../styles/globals.css';
 
 import { Layout } from '@/modules';
 function MyApp({ Component, pageProps }: AppProps) {
+  const [user, setUser] = useState<User>({
+    data: null,
+  });
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    setUser({
+      data: user,
+    });
+  }, []);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserWrapper hook={{ user, setUser }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserWrapper>
   );
 }
 
